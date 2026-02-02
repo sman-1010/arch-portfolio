917c31c5b79af94fe4676dcfe5474eb2e1ffed97
Suleman <sulemann1010@gmail.com>
2026-02-03 00:12:17 +0500

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://m9studios.github.io',
+  base: '/arch-portfolio',
+});
